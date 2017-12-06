0675b3a4c183cfe28c2984e9ea6c61bebbdc7b0d
barophobe <edoconnolly@gmail.com>
2017-12-06 13:25:56 +0000

    first commit, basic class features...

---
+++ constructor_destructor.php
@@ -0,0 +1,25 @@
+<?php
+class Cars {
+
+	Public $wheel_count = 4;
+
+	static $door_count = 4;
+
+
+	function __construct() {
+	echo $this->wheel_count;
+	echo self::$door_count++;
+
+	}
+
+
+}
+$bmw = new Cars();
+$mercedes = new Cars();
+$mercedes2 = new Cars();
+
+
+
+
+
+?>
